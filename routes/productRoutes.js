a07aea69d3139e3cecbd6e8ae00dad58fd75de38
Naveen <naveen@confyans.com>
2025-03-20 11:55:29 +0530

    add comment

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -9,6 +9,6 @@ router.get('/', authProtect, getProducts); // Define list product route
 router.post('/create', upload.single("product_image"), authProtect, validateCreateProduct, createProduct); // Define create product route
 router.put('/edit', upload.single("product_image"), authProtect, validateEditProduct, editProduct); // Define edit product route
 router.delete('/delete', authProtect, deleteProduct); // Define delete product route
-router.get('/user-products', authProtect, getUserProducts); // Define list all user's product route
+router.get('/user-products', authProtect, getUserProducts); // Define list all user's product routes
 
 module.exports = router;
